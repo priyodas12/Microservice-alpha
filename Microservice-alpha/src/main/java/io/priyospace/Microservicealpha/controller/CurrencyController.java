e05abf84a0e22e59b7121f0c25755aea2784e82d
Priyobrato Das <priyodas12@gmail.com>
2020-05-07 01:41:14 +0530

    added own actuator end point

---
+++ Microservice-alpha/src/main/java/io/priyospace/Microservicealpha/controller/CurrencyController.java
@@ -0,0 +1,33 @@
+package io.priyospace.Microservicealpha.controller;
+
+import io.priyospace.Microservicealpha.model.Currency;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Endpoint(id = "currency")
+public class CurrencyController {
+
+    Map<String, Currency> currencyList=new ConcurrentHashMap<>();
+
+    @ReadOperation
+    public Map<String,Currency> getCurrencyDetails(){
+        return currencyList;
+    }
+    @ReadOperation
+    public Currency getCurrency(@Selector String type) {
+        return currencyList.get(type);
+    }
+
+    @WriteOperation
+    public void setCurrency(@Selector String type,int currency) {
+        currencyList.put(type,new Currency(currency));
+    }
+}
---
+++ Microservice-alpha/src/main/java/io/priyospace/Microservicealpha/model/Currency.java
@@ -0,0 +1,18 @@
+package io.priyospace.Microservicealpha.model;
+
+public class Currency {
+
+    int value;
+
+    public Currency(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
